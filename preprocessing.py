358e3091997da1b86d33bff28be176d5b28db9bb
maleehasyed <maleehasyed2006@gmail.com>
2024-11-25 19:57:23 +0500

    preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,27 +1,3 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms, models
-from torch.utils.data import DataLoader
-from sklearn.metrics import classification_report, confusion_matrix
-import numpy as np
-import matplotlib.pyplot as plt
-import torch.nn.utils.prune as prune
-import os
-import seaborn as sns
-from PIL import Image
-from nbconvert import PythonExporter
-import nbformat
-import time
-import streamlit as st
-
-
-
-# Set device (GPU if available, else CPU)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-
 # **Data Preparation**
 
 
